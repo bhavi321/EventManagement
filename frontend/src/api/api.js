0c6ddd894258a34638ad94df67eb6dc9a6d85f6d
balkhaniya-kc <balkhaniya@kcoverseas.com>
2025-10-19 23:20:14 +0530

    added vite url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.API_URL || 'http://localhost:3000/api';
+const API_URL = import.meta.env.VITE_API_URL;
 
 // User/Profile API
 export const getProfiles = async () => {
