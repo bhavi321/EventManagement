7e2576ea9363146e1f5c9012bd0621705c72ab32
balkhaniya-kc <balkhaniya@kcoverseas.com>
2025-10-19 18:56:44 +0530

    changed some components

--- EventManagement/frontend/src/components/EventsList.jsx
+++ EventManagement/frontend/src/components/EventsList.jsx
@@ -4,6 +4,8 @@ import { updateProfile } from '../store/store';
 import { updateProfileTimezone } from '../api/api';
 import { TIMEZONES, getTimezoneLabel } from '../utils/timezones';
 import EventCard from './EventCard';
+import EditEventModal from './EditEventModal';
+import UpdateLogsModal from './UpdateLogsModal';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
@@ -16,34 +18,6 @@ function EventsList({ currentProfile, onEventUpdated }) {
   const dispatch = useDispatch();
   const events = useSelector((state) => state.app.events);
   const profiles = useSelector((state) => state.app.profiles);
-  const [selectedTimezone, setSelectedTimezone] = useState('America/New_York');
-  const [editingEvent, setEditingEvent] = useState(null);
-  const [viewingLogs, setViewingLogs] = useState(null);
-
-  useEffect(() => {
-    if (currentProfile) {
-      setSelectedTimezone(currentProfile.timezone);
-    }
-  }, [currentProfile]);
-
-  const handleTimezoneChange = async (newTimezone) => {
-    setSelectedTimezone(newTimezone);
-    
-    if (currentProfile) {
-      try {
-        const updatedProfile = await updateProfileTimezone(currentProfile._id, newTimezone);
-        dispatch(updateProfile({ id: currentProfile._id, updatedProfile }));
-      } catch (error) {
-        console.error('Error updating timezone:', error);
-      }
-    }
-  };
-
-  const filteredEvents = currentProfile
-    ? events.filter(event => 
-        event.profiles.some(p => p._id === currentProfile._id)
-      )
-    : [];
 
   return (
     <div className="events-card">
@@ -77,26 +51,6 @@ function EventsList({ currentProfile, onEventUpdated }) {
           ))
         )}
       </div>
-
-      {editingEvent && (
-        <EditEventModal
-          event={editingEvent}
-          profiles={profiles}
-          onClose={() => setEditingEvent(null)}
-          onUpdate={() => {
-            setEditingEvent(null);
-            onEventUpdated();
-          }}
-        />
-      )}
-
-      {viewingLogs && (
-        <UpdateLogsModal
-          event={viewingLogs}
-          timezone={selectedTimezone}
-          onClose={() => setViewingLogs(null)}
-        />
-      )}
     </div>
   );
 }
